19078311fa1ff889c1da1fbc3262b3e729ce71e4
Jo. Adly <youssefadly237@gmail.com>
2025-08-20 14:32:12 +0300

    change color layout to vi; add secondory grid colors for random access;

--- colors.lua
+++ colors.lua
@@ -1,17 +1,17 @@
 -- Static color definitions for older versions
 local static_colors = {
-    { name = "Black",   color = 0x000000, buttons = { "x" } },
-    { name = "White",   color = 0xf8f8f2, buttons = { "w" } },
-    { name = "Pink",    color = 0xff79c6, buttons = { "q" } },
-    { name = "Red",     color = 0xff5555, buttons = { "r" } },
-    { name = "Orange",  color = 0xffb86c, buttons = { "o" } },
-    { name = "Yellow",  color = 0xf1fa8c, buttons = { "y" } },
-    { name = "Green",   color = 0x50fa7b, buttons = { "g" } },
-    { name = "Cyan",    color = 0x8be9fd, buttons = { "c" } },
-    { name = "Blue",    color = 0x6272a4, buttons = { "b" } },
-    { name = "Purple",  color = 0xbd93f9, buttons = { "p", "a" } },
+  { name = "Black",  color = 0x000000, buttons = { "x" } },
+  { name = "White",  color = 0xf8f8f2, buttons = { "w" } },
+  { name = "Pink",   color = 0xff79c6, buttons = { "q" } },
+  { name = "Red",    color = 0xff5555, buttons = { "r" } },
+  { name = "Orange", color = 0xffb86c, buttons = { "o" } },
+  { name = "Yellow", color = 0xf1fa8c, buttons = { "y" } },
+  { name = "Green",  color = 0x50fa7b, buttons = { "g" } },
+  { name = "Cyan",   color = 0x8be9fd, buttons = { "c" } },
+  { name = "Blue",   color = 0x6272a4, buttons = { "b" } },
+  { name = "Purple", color = 0xbd93f9, buttons = { "p", "a" } },
 }
 
 return {
-    static_colors = static_colors
+  static_colors = static_colors
 }
